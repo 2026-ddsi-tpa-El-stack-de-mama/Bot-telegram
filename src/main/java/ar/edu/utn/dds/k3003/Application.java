a75e244f8ee16142c1880eb1bbed325ebe2914dc
aculeddu <aculeddu@frba.utn.edu.ar>
2026-08-15 03:40:44 -0300

    Correcciones para que levante

--- src/main/java/ar/edu/utn/dds/k3003/Application.java
+++ src/main/java/ar/edu/utn/dds/k3003/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -11,6 +12,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class Application {
 
   public static void main(String[] args) {
